193c37f571e133c07dbb05636c70b05c69a76f94
N I N K <69116944+ninksazerac@users.noreply.github.com>
2022-10-16 02:49:48 +0700

    ui finished, no link

--- src/pages/ChooseRole/index.js
+++ src/pages/ChooseRole/index.js
@@ -1,23 +1,27 @@
 import React from 'react'
 import { useState, useEffect } from "react";
-import chooserole1 from "../../assets/pics/chooserole1.jpg";
-import chooserole2 from "../../assets/pics/chooserole2.jpg";
+import student from "../../assets/pics/student1.png";
+import tutor from "../../assets/pics/tutor1.png";
 
 export default function ChooseRole() {
     return(
         <div class="flex items-center justify-center h-screen font-body">
     
         <div class="bg-white text-white font-bold rounded-xl border shadow-lg p-10">
-            <div class="font-bold text-4xl text-primary-100 text-center py-2">
+            <div class="font-bold text-6xl text-primary-100 text-center py-2">
                 Who are you?
             </div>
-            <div class="flex flex-row items-center justify-center ">
-                <div class="h-72 w-72 bg-white-100 border-2 shadow-sm rounded-lg text-center ">
-                
-
+            <div class="flex flex-row items-center justify-center mt-4">
+                <div class="h-72 w-72 bg-white-100 border-2 shadow-sm rounded-lg text-center text-3xl p-2 cursor-pointer hover:bg-primary-80 hover:text-white-100 hover:shadow-md">
+                    <img class="ml-10 mt-2 pt-2"
+                    src={student}>
+                    </img>
                     Student
                 </div>
-                <div class="h-72 w-72 bg-white-100 border-2 shadow-sm rounded-lg ml-5 text-center">
+                <div class="h-72 w-72 bg-white-100 border-2 shadow-sm rounded-lg text-center text-3xl p-2 ml-6 cursor-pointer hover:bg-primary-80 hover:text-white-100 hover:shadow-md">
+                    <img class="ml-10 mt-2 pt-2"
+                    src={tutor}>
+                    </img>
                     Tutor
                 </div>
             </div>
