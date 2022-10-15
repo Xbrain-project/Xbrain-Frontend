81ee8975732f58c7a763d51e7632de0122eea5ff
N I N K <69116944+ninksazerac@users.noreply.github.com>
2022-10-15 19:09:01 +0700

    ui finished

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,14 +1,85 @@
 import React from 'react'
 import { useState, useEffect } from "react";
+import tutorpic from "../../assets/pics/login1.jpg";
 
 export default function Login() {
     return(
+        <div class="flex flex-row">
+        <img 
+            class="h-screen object-contain opacity-1" 
+            src={tutorpic}>
+            </img>
+
         <div
-            class="bg-primary-80 px-2 sm:px-4 py-2.5 fixed w-full z-20 top-0 left-0">
-           <div 
-                class="w-full bg-primary-100 flex flex-row">
+            class="container px-6 py-12 h-screen bg-white-100 w-full">
+                <div 
+                class="flex justify-center items-center flex-wrap h-full g-6 text-gray-800">
+                    <div 
+                    class=" w-9/12 p-10">
+                    {/* <!-- Form --> */}
+                    <form>
+                    <div class="font-body text-center font-bold text-6xl text-primary-100 py-5">
+                        Login
                     </div>
-        </div>
+                    {/* <!-- Email input --> */}
+                    <div class="mb-6">
+                    <label 
+                        class="block font-semibold text-lg md:text-left mb-1 md:mb-0 pr-4 py-2" for="inline-full-name">
+                        Email
+                        </label>
+                        <input
+                        type="text"
+                        class="form-control block w-full px-4 py-2 bg-white-100 bg-clip-padding border border-solid border-gray-50 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+                        />
+                    </div>
                     
+
+                    {/* <!-- Password input --> */}
+                    <div class="mb-6">
+                    <label 
+                        class="block font-semibold text-lg md:text-left mb-1 md:mb-0 pr-4 py-2" for="inline-full-name">
+                        Password
+                        </label>
+                        <input
+                        type="password"
+                        class="form-control block w-full px-4 py-2 bg-white-100 bg-clip-padding border border-solid border-gray-50 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+                        placeholder="******************"
+                        />
+                        
+                    </div>
+
+                    <div class="py-2 grid justify-items-end">
+                        <a
+                        href="/"
+                        class="font-body text-gray-100 hover:text-primary-100 hover:underline duration-200 transition ease-in-out"
+                        >Forgot password?
+                        </a>
+                    </div>
+
+                    {/* <!-- Submit button --> */}
+                    <button
+                        type="submit"
+                        class="inline-block px-7 py-3 bg-primary-80 text-white-100 font-body font-semibold text-lg leading-snug rounded-2xl shadow-md hover:bg-primary-100 hover:shadow-lg focus:bg-primary-100 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-primary-100 active:shadow-lg transition duration-150 ease-in-out w-full"
+                        data-mdb-ripple="true"
+                        data-mdb-ripple-color="light"
+                    >
+                        Login
+                    </button>
+                    <div class="py-2 grid justify-items-center">
+                        <p class="font-body text-gray-100">
+                    Don’t have an account?
+                        <a
+                        href="/"
+                        class="font-semibold text-primary-80 hover:text-primary-100 hover:underline duration-200 transition ease-in-out pl-2"
+                        >Sign up
+                        </a>
+                        </p>
+                    </div>
+                    </form>
+                    </div>
+                </div>
+
+            </div>   
+        </div> 
     );
 }
