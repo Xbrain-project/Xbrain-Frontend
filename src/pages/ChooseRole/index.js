6b54f31fdf511e4c762359252f9ab1bf610e478a
N I N K <69116944+ninksazerac@users.noreply.github.com>
2022-10-16 02:09:33 +0700

    no pics

--- src/pages/ChooseRole/index.js
+++ src/pages/ChooseRole/index.js
@@ -5,20 +5,24 @@ import chooserole2 from "../../assets/pics/chooserole2.jpg";
 
 export default function ChooseRole() {
     return(
-        <div class="flex flex-row">
-            <div class="w-full h-screen ">
-                <img 
-                class="w-full h-screen bg-cover opacity-75 hover:bg-primary-80"
-                src={chooserole1}>
-                    
-                </img>
+        <div class="flex items-center justify-center h-screen font-body">
+    
+        <div class="bg-white text-white font-bold rounded-xl border shadow-lg p-10">
+            <div class="font-bold text-4xl text-primary-100 text-center py-2">
+                Who are you?
             </div>
-            <div class="w-full h-screen ">
-            <img class="w-full h-screen bg-cover opacity-75 hover:opacity-100"
-                src={chooserole2}>
+            <div class="flex flex-row items-center justify-center ">
+                <div class="h-72 w-72 bg-white-100 border-2 shadow-sm rounded-lg text-center ">
+                
 
-                </img>
+                    Student
+                </div>
+                <div class="h-72 w-72 bg-white-100 border-2 shadow-sm rounded-lg ml-5 text-center">
+                    Tutor
+                </div>
             </div>
-            </div>
+        </div>
+        
+      </div>
         );
 }
