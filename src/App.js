2e1650d9187e3a0cfa5e77b8eece04c9d7de4922
NuntNonthicha <69186692+NuntNonthicha@users.noreply.github.com>
2022-10-15 22:17:12 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,20 +1,19 @@
 import React from "react";
-import './App.css';
+import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-// import NavbarStudent from "./components/navbar/NavbarStudent";
-// import NavbarTutor from "./components/navbar/NavbarTutor";
-
+import NavbarStudent from "./components/navbar/NavbarStudent";
+import NavbarTutor from "./components/navbar/NavbarTutor";
 
 function App() {
   return (
     <BrowserRouter>
-    <div>
-      <Routes>
-        {/* <Route path="/" element={<NavbarStudent/>} />
-        <Route path="/1" element={<NavbarTutor/>} /> */}
-      </Routes>
+      <div>
+        <Routes>
+          <Route path="/" element={<NavbarStudent />} />
+          <Route path="/1" element={<NavbarTutor />} />
+        </Routes>
       </div>
-      </BrowserRouter>
+    </BrowserRouter>
   );
 }
 
