f82b75fc4320b91224230be999ff341ddc221c5e
N I N K <69116944+ninksazerac@users.noreply.github.com>
2022-10-16 01:05:39 +0700

    :(

--- src/pages/ChooseRole/index.js
+++ src/pages/ChooseRole/index.js
@@ -1,11 +1,24 @@
 import React from 'react'
 import { useState, useEffect } from "react";
-import forgotpass from "../../assets/pics/forgotpass1.jpg";
+import chooserole1 from "../../assets/pics/chooserole1.jpg";
+import chooserole2 from "../../assets/pics/chooserole2.jpg";
 
 export default function ChooseRole() {
     return(
         <div class="flex flex-row">
-            fdsf
+            <div class="w-full h-screen ">
+                <img 
+                class="w-full h-screen bg-cover opacity-75 hover:bg-primary-80"
+                src={chooserole1}>
+                    
+                </img>
+            </div>
+            <div class="w-full h-screen ">
+            <img class="w-full h-screen bg-cover opacity-75 hover:opacity-100"
+                src={chooserole2}>
+
+                </img>
+            </div>
             </div>
         );
 }
