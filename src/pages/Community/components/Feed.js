06bd31935902eb16f4713b3804dfb09a701c90b9
NuntNonthicha <69186692+NuntNonthicha@users.noreply.github.com>
2022-11-17 06:05:06 +0700

    create file commu : not have data

---
+++ src/pages/Community/components/Feed.js
@@ -0,0 +1,7 @@
+import React, { useState, useEffect } from "react";
+
+const Feed = () => {
+  return <div className="md:mt-32 md:mb-32">Feed</div>;
+};
+
+export default Feed;
--- src/pages/Community/index.js
+++ src/pages/Community/index.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Blog from "./components/Blog";
+import Feed from "./components/Feed";
 
 const Community = () => {
   return (
     <div className="w-full justify-center my-12">
       <Blog />
+      {/* <Feed/> */}
     </div>
   );
 };
