90b8be89f79f6f2797c2337b9643211b049e67b8
NuntNonthicha <69186692+NuntNonthicha@users.noreply.github.com>
2022-11-16 23:30:46 +0700

    fix applyTutor but handleApprove & Reject not complete

---
+++ src/pages/ApplyTutor/components/TestData.js
@@ -0,0 +1,46 @@
+import React from "react";
+
+const TestData = [
+  {
+    id: 1,
+    student: "nunt",
+    college: "kmitl",
+    subject: "คณิตศาสตร์",
+    type: "ติวเพิ่มเกรด",
+    wage: 250,
+  },
+  {
+    id: 2,
+    student: "ploy",
+    college: "kmitl",
+    subject: "คณิตศาสตร์",
+    type: "ติวเพิ่มเกรด",
+    wage: 250,
+  },
+  {
+    id: 3,
+    student: "nink",
+    college: "kmitl",
+    subject: "คณิตศาสตร์",
+    type: "ติวเพิ่มเกรด",
+    wage: 250,
+  },
+  {
+    id: 4,
+    student: "phe",
+    college: "kmitl",
+    subject: "คณิตศาสตร์",
+    type: "ติวเพิ่มเกรด",
+    wage: 250,
+  },
+  {
+    id: 5,
+    student: "junior",
+    college: "kmitl",
+    subject: "คณิตศาสตร์",
+    type: "ติวเพิ่มเกรด",
+    wage: 250,
+  },
+];
+
+export default TestData;
