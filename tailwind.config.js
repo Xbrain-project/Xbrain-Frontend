d98522705bfe3ee1c4828f20c6014f21c2d83b68
NuntNonthicha <69186692+NuntNonthicha@users.noreply.github.com>
2022-11-20 22:26:36 +0700

    fix merge nink : install package

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,35 +4,19 @@ module.exports = {
     extend: {
       colors: {
         primary: {
-          50: "#F2BCFB", //logo
-          80: "#855CF8", //default
-          100: "#503795", //hover
+          50: "#F2BCFB",
+          80: "#855CF8",
+          100: "#503795",
         },
         white: {
           100: "#FFFFFF",
         },
         gray: {
-          50: "#D9D9D9", //underline
-          100: "#4F4F4F", //disable
-          700: "#374151", //textbox
+          50: "#D9D9D9",
+          100: "#4F4F4F",
         },
-
         black: {
           100: "#000000",
-        },
-      },
-
-
-      fontFamily: {
-        body: ["sans-serif", "Noto Sans Thai"],
-
-        black: {
-          100: "#000000",
-
-      
-        fontFamily: {
-          body: ["sans-serif", "Noto Sans Thai"],
-
         },
       },
 
