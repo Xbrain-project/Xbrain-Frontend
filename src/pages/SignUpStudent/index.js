a5e211ac08569da0958d043a4151bb2962926d61
N I N K <69116944+ninksazerac@users.noreply.github.com>
2022-11-20 20:06:51 +0700

    fix useForm Signup

--- src/pages/SignUpStudent/index.js
+++ src/pages/SignUpStudent/index.js
@@ -1,130 +1,156 @@
-import React, {useState} from 'react'
+import React from 'react';
+import { useForm } from 'react-hook-form';
 
 
 export default function SignUpStudent() {
-    //เก็บค่า form sign up
-    const [form, setForm] = useState({ 
-        email: "",
-        password: "",
-        cfpassword: "",
-    })
+    // //เก็บค่า form sign up
+    // const [form, setForm] = useState({ 
+    //     email: "",
+    //     password: "",
+    //     cfpassword: "",
+    // })
 
-    // change
-    const handleChange = (e) => {  
-        setForm({ ...form, 
-           [e.target.name]: e.target.value });
+    // // change
+    // const handleChange = (e) => {  
+    //     setForm({ ...form, 
+    //        [e.target.name]: e.target.value });
          
-    };
+    // };
 
-    //submit
-    const handleSubmit = (e) => { 
-        e.preventDefault()
+    // //submit
+    // const handleSubmit = (e) => { 
+    //     e.preventDefault()
         
-        //check blank required
-        if(form.email !== '' && form.password !== ''){
-            const user={form}
-            console.log(user)
-        } else if (form.email === '' && form.password !== ''){
-            alert('Email is required.');
-        } else if (form.email !== '' && form.password === ''){
-            alert('Password is required.');
-        } else {
-            alert('Email and a password are required.');
-        }
+    //     //check blank required
+    //     if(form.email !== '' && form.password !== ''){
+    //         const user={form}
+    //         console.log(user)
+    //     } else if (form.email === '' && form.password !== ''){
+    //         alert('Email is required.');
+    //     } else if (form.email !== '' && form.password === ''){
+    //         alert('Password is required.');
+    //     } else {
+    //         alert('Email and a password are required.');
+    //     }
         
           
+    // }
+    const { register, handleSubmit, watch, formState: { errors } } = useForm();
+    const onSubmit = data => {
+        console.log(data);
     }
+    // {alert(JSON.stringify(data))}
+
+    console.log(watch("example"));
 
     return(    
-        <div class="bg-purple-200 w-full h-full">
+        <div className="bg-purple-200 w-full h-full">
         <div 
-            class="flex justify-center items-center px-80 py-12 h-screen font-body">
-                <div class=" container justify-center items-center mx-40 bg-white-100 rounded-lg shadow-lg border border-gray-50">
+            className="flex justify-center items-center px-80 py-12 h-screen font-body">
+                <div className=" container justify-center items-center mx-40 bg-white-100 rounded-lg shadow-lg border border-gray-50">
                 <div 
-                    class="flex justify-center items-center flex-wrap h-full g-6 text-gray-800">
+                    className="flex justify-center items-center flex-wrap h-full g-6 text-gray-800">
                     <div 
-                    class=" w-9/12 p-10">
+                    className=" w-9/12 p-10">
                     {/* <!-- Form --> */}
-                    <form>
-                    <div class="text-center font-bold text-6xl text-primary-80 py-5">
+                    <form onSubmit={handleSubmit(onSubmit)}>
+                    <div className="text-center font-bold text-6xl text-primary-80 py-5">
                         Register
                     </div>
                     
-                        <div class="justify-center flex flex-row ">
+                        <div className="justify-center flex flex-row ">
                             <p 
-                                class="text-lg text-gray-400 pr-2">
+                                className="text-lg text-gray-400 pr-2">
                                     Role by </p>
                             <p 
-                                class="text-lg font-semibold text-white-100 bg-primary-100 pl-1 pr-1 rounded-lg">
+                                className="text-lg font-semibold text-white-100 bg-primary-100 pl-1 pr-1 rounded-lg">
                                     Student</p>
                         </div>
                     
                     {/* <!-- Email input --> */}
-                    <div class="mb-6">
+                    <div className="mb-6">
                     <label 
-                        class="block font-semibold text-lg md:text-left mb-1 md:mb-0 pr-4 py-2" for="inline-full-name">
+                        className="block font-semibold text-lg md:text-left mb-1 md:mb-0 pr-4 py-2" for="inline-full-name">
                         Email
                         </label>
                         <input
                         type="email"
                         name="email"
-                        class="form-control block w-full px-4 py-2 bg-white-100 bg-clip-padding border border-solid border-gray-50 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+                        style={{borderColor: errors.email ? "red" :"", }}
+                        className="form-control block w-full px-4 py-2 bg-white-100 bg-clip-padding border border-solid border-gray-50 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
                         placeholder='you@gmail.com'
-                        value={form.email}
-                        onChange={handleChange}
+                        {...register("email", {required : 'Email is required'})}
                         />
+                            {errors.email && <span style={{color : "red"}}>
+                            {errors.email.message}
+                            </span>}
                     </div>
                     
 
                     {/* <!-- Password input --> */}
-                    <div class="mb-6">
+                    <div className="mb-6">
                     <label 
-                        class="block font-semibold text-lg md:text-left mb-1 md:mb-0 pr-4 py-2" for="inline-full-name">
+                        className="block font-semibold text-lg md:text-left mb-1 md:mb-0 pr-4 py-2" for="inline-full-name">
                         Password
                         </label>
                         <input
                         type="password"
                         name="password"
-                        class="form-control block w-full px-4 py-2 bg-white-100 bg-clip-padding border border-solid border-gray-50 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+                        style={{borderColor: errors.password ? "red" :"", }}
+                        className="form-control block w-full px-4 py-2 bg-white-100 bg-clip-padding border border-solid border-gray-50 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
                         placeholder="******************"
-                        value={form.password}
-                        onChange={handleChange}
+                        {...register("password", {required : 'Password is required',
+                            minLength:{
+                                value: 8,
+                                message: 'Password must be at least 8 characters.'
+                            }
+                            })}
                         />
-                        
+                            {errors.password && <span style={{color : "red"}}>
+                            {errors.password.message}
+                            </span>}
                     </div>
 
                     {/* <!-- Confirm Password input --> */}
-                    <div class="mb-6">
+                    <div className="mb-6">
                     <label 
-                        class="block font-semibold text-lg md:text-left mb-1 md:mb-0 pr-4 py-2" for="inline-full-name">
+                        className="block font-semibold text-lg md:text-left mb-1 md:mb-0 pr-4 py-2" for="inline-full-name">
                         Confirm Password
                         </label>
                         <input
                         type="password"
                         name="cfpassword"
-                        class="form-control block w-full px-4 py-2 bg-white-100 bg-clip-padding border border-solid border-gray-50 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+                        style={{borderColor: errors.cfpassword ? "red" :"", }}
+                        className="form-control block w-full px-4 py-2 bg-white-100 bg-clip-padding border border-solid border-gray-50 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
                         placeholder="******************"
-                        value={form.cfpassword}
-                        onChange={handleChange}
+                        {...register("cfpassword", {required : 'Confirm Password is required',
+                            minLength:{
+                                value: 8,
+                                message: 'ConFirm Password must be at least 8 characters.'
+                            },
+                            validate: (value) => value === watch('password') || "Password do not match.",
+                            })}
                         />
-                        
+                            {errors.cfpassword && <span style={{color : "red"}}>
+                            {errors.cfpassword.message}
+                            </span>}
                     </div>
 
                     {/* <!-- Submit button --> */}
                     <button
                         type="submit"
-                        class="inline-block px-7 py-3 bg-primary-80 text-white-100 font-body font-semibold text-lg leading-snug rounded-2xl shadow-md hover:bg-primary-100 hover:shadow-lg focus:bg-primary-100 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-primary-100 active:shadow-lg transition duration-150 ease-in-out w-full"
+                        className="inline-block px-7 py-3 bg-primary-80 text-white-100 font-body font-semibold text-lg leading-snug rounded-2xl shadow-md hover:bg-primary-100 hover:shadow-lg focus:bg-primary-100 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-primary-100 active:shadow-lg transition duration-150 ease-in-out w-full"
                         data-mdb-ripple="true"
                         data-mdb-ripple-color="light"
                     >
                         Sign up
                     </button>
-                    <div class="py-2 grid justify-items-center">
-                        <p class="font-body text-gray-100">
+                    <div className="py-2 grid justify-items-center">
+                        <p className="font-body text-gray-100">
                     Have an account?
                         <a
                         href="/"
-                        class="font-semibold text-primary-80 hover:text-primary-100 hover:underline duration-200 transition ease-in-out pl-2"
+                        className="font-semibold text-primary-80 hover:text-primary-100 hover:underline duration-200 transition ease-in-out pl-2"
                         >Log in
                         </a>
                         </p>
