4399a0e7fef18bca151d0de3d13472cb271356a3
J-Punnawich <newandovergame@gmail.com>
2022-11-23 05:46:35 +0700

    fix profile

--- src/api/user.js
+++ src/api/user.js
@@ -14,11 +14,23 @@ export const signUp = async (value) => {
   return await axios.post("http://localhost:8080/api/register", value);
 };
 
-// TODO:
-export const getUserById = async (id) => {
-  return await axios.get("http://localhost:8080/api/posts/" + id);
+// export const getUserById = async (id) => {
+//   return await axios.get("http://localhost:8080/api/posts/" + id);
+// };
+
+export const getTutor = async (id) => {
+  return await axios.get("http://localhost:8080/getTeacherById/" + id);
 };
 
-export const updateUser = async (value, id) => {
-  return await axios.put("http://localhost:8080/posts/edit-post/" + id, value);
+export const getStudent = async (id) => {
+  return await axios.get("http://localhost:8080/getStudentById/" + id);
+};
+
+// TODO:
+export const updateTutor = async (value) => {
+  return await axios.put("http://localhost:8080/updateTeacher", value);
+};
+
+export const updateStudent = async (value) => {
+  return await axios.put("http://localhost:8080/updateStudent", value);
 };
